aa10ff8a5f3ca7cf2f9e29295480026581c9ee7e
Arildo Oliveira <arildoosilva@gmail.com>
2015-02-26 10:04:32 -0300

    Initial commit

---
+++ filesorter.py
@@ -0,0 +1,13 @@
+"""
+This is a simple script to sort media files and move them to their folders.
+
+HOW TO USE:
+1. change the source and destination strings.
+    On Windows - C:/the/path/
+    On Linux - /the/path/
+2. Copy the if statement, changing the key and the folder.
+3. Run
+"""
+
+import os
+import shutil
