de38598f20c5e7b5715eeba8d0d900676f907d4e
Arildo Oliveira <arildoosilva@gmail.com>
2015-02-26 10:47:29 -0300

    v2
    Using a list for iteration

--- filesorter.py
+++ filesorter.py
@@ -1,12 +1,11 @@
 """
 This is a simple script to sort files and move them to their folders.
-
 HOW TO USE:
 1. change the source and destination strings.
-    On Windows - drive_letter:/the/path/
-    On Linux - /the/path/
-2. Copy the if statement, changing the key and the folder.
-3. Run
+ On Windows - drive_letter:/the/path/
+ On Linux - /the/path/
+2. Add values to the list, the first one is the key string in the file, the second is the destination folder.
+3. Run.
 """
 
 import os
@@ -15,11 +14,12 @@ import shutil
 source='C:/path/to/source/'
 destination='C:/path/to/destination/'
 
+the_list=[['photo', 'My Pictures'], ['video', 'My Videos']]
+
 for root, dirs, files in os.walk(source, topdown=False):
-    for name in dirs:
-        print(name)
     for name in files:
         f_name=os.path.join(root, name)
         print(f_name)
-        if 'key' in (name):
-            shutil.move(f_name, destination+'folder')
+        for x in the_list:
+            if x[0] in (name):
+                shutil.move(f_name, destination+x[1])
