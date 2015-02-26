f8170c054a0abd674f84e10c88c85d61566ac3a8
Arildo Oliveira <arildo.o.s@outlook.com>
2015-02-26 11:06:59 -0300

    Updated script

--- filesorter.py
+++ filesorter.py
@@ -16,10 +16,14 @@ destination='C:/path/to/destination/'
 
 the_list=[['photo', 'My Pictures'], ['video', 'My Videos']]
 
-for root, dirs, files in os.walk(source, topdown=False):
-    for name in files:
-        f_name=os.path.join(root, name)
-        print(f_name)
-        for x in the_list:
-            if x[0] in (name):
-                shutil.move(f_name, destination+x[1])
+try:
+    for root, dirs, files in os.walk(source):
+        for name in dirs:
+            print(name)
+        for name in files:
+            f_name=os.path.join(root, name)
+            for x in the_list:
+                if x[0].lower() in (name.lower()):
+                    shutil.move(f_name, destination+x[1])
+except Exception:
+    pass
